6b595ed7d3f703c9727f06bee5f8e207dbc134b3
Gavin Payne <gavinapayne@gmail.com>
2025-09-14 12:51:11 +0400

    v1.0.8: redis upgrades

--- src/CloudinaryFileContent/node/index.ts
+++ src/CloudinaryFileContent/node/index.ts
@@ -7,7 +7,7 @@ function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
   
   return {
-    packageVersion: "1.0.6",
+    packageVersion: "1.0.7",
     type: NODE_TYPE,
     name: "Cloudinary File Content",
     description: "Get content and metadata for a Cloudinary file",
--- src/CloudinaryFiles/node/index.ts
+++ src/CloudinaryFiles/node/index.ts
@@ -7,7 +7,7 @@ function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
 
   return {
-    packageVersion: "1.0.6",
+    packageVersion: "1.0.7",
     type: NODE_TYPE,
     name: "Cloudinary Files",
     description: "List files from a Cloudinary folder",
