242b18dd561896389f27400754d32d0082dbc87a
Gavin Payne <gavinapayne@gmail.com>
2025-09-16 17:57:45 +0400

    v1.0.11

--- src/CloudinaryFileContent/node/index.ts
+++ src/CloudinaryFileContent/node/index.ts
@@ -7,7 +7,7 @@ function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
   
   return {
-    packageVersion: "1.0.9",
+    packageVersion: "1.0.10",
     type: NODE_TYPE,
     name: "Cloudinary File Content",
     description: "Get content and metadata for a Cloudinary file",
--- src/CloudinaryFiles/node/index.ts
+++ src/CloudinaryFiles/node/index.ts
@@ -7,7 +7,7 @@ function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
 
   return {
-    packageVersion: "1.0.9",
+    packageVersion: "1.0.10",
     type: NODE_TYPE,
     name: "Cloudinary Files",
     description: "List files from a Cloudinary folder",
