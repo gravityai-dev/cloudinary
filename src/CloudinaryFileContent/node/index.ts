6e08ec41ccc896b8fde7f6a6ab9d11465ead0387
Gavin Payne <gavinapayne@gmail.com>
2025-09-16 21:16:24 +0400

    v1.0.13

--- src/CloudinaryFileContent/node/index.ts
+++ src/CloudinaryFileContent/node/index.ts
@@ -7,7 +7,7 @@ function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
   
   return {
-    packageVersion: "1.0.11",
+    packageVersion: "1.0.12",
     type: NODE_TYPE,
     name: "Cloudinary File Content",
     description: "Get content and metadata for a Cloudinary file",
